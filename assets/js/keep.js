a16d5931d03d0abcf02e23c166eb5b3a0cc4e3f5
Thankgod222 <thankgodogbonna01@gmail.com>
2022-09-15 01:10:08 +0100

    creating pages

--- assets/js/keep.js
+++ assets/js/keep.js
@@ -1,3 +1,25 @@
+
+
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+  // LINK ACTIVE WORK
+
+const linkWork = document.querySelectorAll(".one");
+
+function activeWork() {
+  linkWork.forEach(l => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+linkWork.forEach(l => l.addEventListener("click", activeWork));
+
+
+
+$(function () {
+  $(document).scroll(function () {
+      var $nav = $("#navBar");
+      $nav.toggleClass("scrolled", $(this).scrollTop() > 30);
+  });
+});
+
